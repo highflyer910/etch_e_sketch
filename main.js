d1f38ad856411ee64d937132cb1d57c85e7f1655
highflyer910 <tmush2014@agruni.edu.ge>
2017-02-06 21:19:45 +0400

    done

--- main.js
+++ main.js
@@ -1,36 +1,61 @@
 $(document).ready(function() {
-	var colors = false;
-	drawBoard();
+  var rainbow = false;
+  drawBoard();
 
-	var $cell = $('.cell'),
-	    $allButtons = $('button'),
-	    $blackButton = $('#black'),
-	    $randomButton = $('#random'),
-	    $colorsButton = $('#colors');
+  var $cell = $('.cell'),
+    $allButtons = $('button'),
+    $blackButton = $('#black'),
+    $randomButton = $('#random'),
+    $rainbowButton = $('#rainbow');
 
+  $cell.on('mouseover', function() {
+    if (rainbow == false) {
+      $(this).css('background-color', color);
+    } else {
+      $(this).css('background-color', getRandomColor());
+    }
 
-	    $cell.on('mouseover' function() {
-	    	if(colors == false) {
-	    		$(this).css('background-color', color);
-	    	} else {
-	    		$(this).css('background-color', getRandomColor());
-	    	}
-	    });
+  });
 
-	    $blackButton.on('click', function() {
-	    	color = 'black';
-	    	colors = false;
-	    	makeActive($blackButton, $allButtons);
-	    });
+  $blackButton.on('click', function() {
+    color = 'black';
+    rainbow = false;
+    makeActive($blackButton, $allButtons);
+  });
 
-	    $randomButton.on('click', function() {
-	    	color = getRandomColor();
-            colors = false;
-            makeActive($randomButton, $allButtons);
-	    });
+  $randomButton.on('click', function() {
+    color = getRandomColor();
+    rainbow = false;
+    makeActive($randomButton, $allButtons);
+  });
 
-	    $colorsButton.on('click', function() {
-	    	colors != colors;
-	    	makeActive($colorsButton, $allButtons);
-	    });
+  $rainbowButton.on('click', function() {
+    rainbow = !rainbow;
+    makeActive($rainbowButton, $allButtons);
+  });
 });
+
+function drawBoard() {
+  var numberOfRows = 10;
+  var numberOfColumns = 20;
+  for (var i = 0; i < numberOfRows; i++) {
+    $('#board').append("<div class=\"row\"><\div>");
+    for (var c = 0; c < numberOfColumns; c++) {
+      $('.row:last-of-type').append("<div class=\"cell\"><\div>");
+    }
+  }
+}
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function makeActive(button, $allButtons) {
+  $allButtons.removeClass('active');
+  button.addClass('active');
+}
